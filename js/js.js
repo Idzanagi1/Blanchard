b27d4a3e4fe1161f1060c38429b016cd13f6c4aa
danila_ledovskoi <d_l_94@mail.ru>
2023-02-19 01:10:08 +0300

    made changes

--- js/js.js
+++ js/js.js
@@ -12,24 +12,25 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+history.pushState("", document.title, window.location.pathname);
+
 const element = document.querySelector("select");
 const choices = new Choices(element, {
   searchEnabled: false,
   itemSelectText: "  ",
 });
 
+var selector = document.querySelector("input[type='tel']");
+var im = new Inputmask("+7 (999) 999-99-99");
+im.mask(selector);
+
 new JustValidate(".map__form", {
   colorWrong: "#D11616",
   rules: {
-    name: {
-      required: true,
-      minLenght: 2,
-      maxLenght: 15,
-    },
     tel: {
       required: true,
       function: () => {
-        const phone = selector.Inputmask.unmaskedvalue();
+        const phone = selector.inputmask.unmaskedvalue();
         return Number(phone) && phone.length === 10;
       },
     },
@@ -67,6 +68,7 @@ function init() {
   );
 
   myMap.geoObjects.add(myPlacemark);
+  myMap.behaviors.disable(["scrollZoom"]);
 }
 
 tippy("#myButton1", {
